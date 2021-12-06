5963b4399e553a4c40be2fb48c2c371d3445fda5
Nick-Kolchugin <drnik71@gmail.com>
2021-12-07 01:25:32 +0600

    Work with Fragments(dont work actually)

---
+++ app/src/main/java/com/example/findfilm/DetailsFragment.kt
@@ -0,0 +1,49 @@
+package com.example.findfilm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.fragment.app.Fragment
+import com.example.findfilm.model.Film
+
+class DetailsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        //ВНИМАНИЕ тут нужно всегда заменять на похожее надувание
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //находим все нам нужные комплектующие
+        val title = view.findViewById<Toolbar>(R.id.details_toolbar)
+        val poster = view.findViewById<ImageView>(R.id.details_poster)
+        val description = view.findViewById<TextView>(R.id.details_description)
+        val container = view.findViewById<CoordinatorLayout>(R.id.details_container)
+
+        //получаем наш фильм через переданный бандл
+        val film = arguments?.get("film") as Film
+
+        //устанавливаем заголовок
+        title.title = film.title
+
+        //устанавливаем постер
+        poster.setImageResource(film.poster)
+
+        //устанавливаем описание
+        description.text = film.description
+    }
+
+}
--- app/src/main/java/com/example/findfilm/MainActivity.kt
+++ app/src/main/java/com/example/findfilm/MainActivity.kt
@@ -1,67 +1,27 @@
 package com.example.findfilm
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.findfilm.model.Film
-import com.example.findfilm.model.FilmListRecyclerAdapter
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import java.text.FieldPosition
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var filmsAdapter: FilmListRecyclerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fragment_placeholder, HomeFragment())
+            .addToBackStack(null)
+            .commit()
+
         val topAppBar = findViewById<MaterialToolbar>(R.id.top_app_bar)
         val navToolBar = findViewById<BottomNavigationView>(R.id.bottom_nav_bar)
-        val listOfFilms = listOf(
-            Film(getString(R.string.shang_chi_title), R.drawable.shang_chi_poster, getString(R.string.shang_chi_desc)),
-            Film(getString(R.string.freeguy_title), R.drawable.freeguy_poster, getString(R.string.freeguy_desc)),
-            Film(getString(R.string.dune_part1_title), R.drawable.dune_poster, getString(R.string.dune_part1_desc)),
-            Film(getString(R.string.house_of_gucci_title), R.drawable.house_of_gucci_poster, getString(R.string.house_of_gucci_desc)),
-            Film(getString(R.string.venom2_title), R.drawable.venom2_poster, getString(R.string.venom2_desc)),
-            Film(getString(R.string.ghostbusters_afterlife_title), R.drawable.ghostbusters_afterlife_poster, getString(R.string.ghostbusters_afterlife_desc)),
-            Film(getString(R.string.last_night_in_soho_title), R.drawable.last_night_in_soho_poster, getString(R.string.last_night_in_soho_desc)),
-            Film(getString(R.string.no_time_to_die_title), R.drawable.no_time_to_die_poster, getString(R.string.no_time_to_die_desc)),
-            Film(getString(R.string.cruella_title), R.drawable.cruella_poster, getString(R.string.cruella_desc)),
-            Film(getString(R.string.mortal_kombat_title), R.drawable.mortal_kombat_poster, getString(R.string.mortal_kombat_desc)),
-        )
-        val mainRecycler = findViewById<RecyclerView>(R.id.main_recycler_view)
 
-        mainRecycler.apply {
-
-            filmsAdapter = FilmListRecyclerAdapter(object: FilmListRecyclerAdapter.OnClickListener{
-                override fun click(film: Film) {
-                    //Создаем бандл и кладем туда объект с данными фильма
-                    val bundle = Bundle()
-                    //Первым параметром указывается ключ, по которому потом будем искать, вторым сам
-                    //передаваемый объект
-                    bundle.putParcelable("film", film)
-                    //Запускаем наше активити
-                    val intent = Intent(this@MainActivity, DetailsActivity::class.java)
-                    //Прикрепляем бандл к интенту
-                    intent.putExtras(bundle)
-                    //Запускаем наше активити через интент
-                    startActivity(intent)
-                }
-            })
-
-            adapter = filmsAdapter
-
-            layoutManager = LinearLayoutManager(this@MainActivity)
-
-            addItemDecoration(TopSpacingItemDecoration(8))
-
-            filmsAdapter.addItems(listOfFilms)
-        }
 
 
         topAppBar.setNavigationOnClickListener {
@@ -98,5 +58,22 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+    }
+
+    fun launchDetailsFragment(film: Film){
+        //создаем посылку
+        val bundle = Bundle()
+        //кладем наш фильм в посылку
+        bundle.putParcelable("film", film)
+        //кладем фрагмент с деталями в переменную
+        val fragment = DetailsFragment()
+        //прикрепляем нашу посылку к фрагменту
+        fragment.arguments = bundle
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_placeholder, fragment)
+            .addToBackStack(null)
+            .commit()
     }
 }
