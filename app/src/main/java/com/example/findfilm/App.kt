c634663d184fa2951fc196afb6e588630801b381
Nick-Kolchugin <drnik71@gmail.com>
2022-08-11 13:45:48 +0600

    Успешно подключен Dagger2 на версии 2.40.5

--- app/src/main/java/com/example/findfilm/App.kt
+++ app/src/main/java/com/example/findfilm/App.kt
@@ -5,7 +5,9 @@ import android.content.Context
 import com.example.findfilm.data.ApiConstants
 import com.example.findfilm.data.MainRepository
 import com.example.findfilm.data.TmdbApi
+import com.example.findfilm.di.AppComponent
 import com.example.findfilm.di.DI
+import com.example.findfilm.di.DaggerAppComponent
 import com.example.findfilm.domain.Interactor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -17,21 +19,19 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
-class App: Application() {
+class App : Application() {
+    lateinit var dagger: AppComponent
 
     override fun onCreate() {
         super.onCreate()
-        startKoin {
-            //Прикрепляем контекст
-            androidContext(this@App)
-            //(Опционально) подключаем зависимость
-            androidLogger()
-            //Инициализируем модули
-            modules(listOf(DI.mainModule))
-        }
+        instance = this
+        //Создаем компонент
+        dagger = DaggerAppComponent.create()
 
     }
 
-
-
+    companion object {
+        lateinit var instance: App
+            private set
+    }
 }
--- app/src/main/java/com/example/findfilm/viewmodel/HomeFragmentViewModel.kt
+++ app/src/main/java/com/example/findfilm/viewmodel/HomeFragmentViewModel.kt
@@ -2,17 +2,22 @@ package com.example.findfilm.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.findfilm.App
 import com.example.findfilm.domain.Film
 import com.example.findfilm.domain.Interactor
 import org.koin.core.KoinComponent
 import org.koin.core.inject
 import org.koin.android.ext.android.inject
+import javax.inject.Inject
 
 class HomeFragmentViewModel : ViewModel(), KoinComponent {
     val filmsListLiveData : MutableLiveData<List<Film>> = MutableLiveData()
-    private val interactor: Interactor by inject()
+    //Инициализируем интерактор
+    @Inject
+    lateinit var interactor: Interactor
 
     init {
+        App.instance.dagger.inject(this)
         interactor.getFilmsFromApi(1, object : ApiCallback {
             override fun onSuccess(films: List<Film>) {
                 filmsListLiveData.postValue(films)
